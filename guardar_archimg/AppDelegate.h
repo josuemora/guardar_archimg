574166a7fb1251427d43a111fc8f49785693b8cc
josuemora <josue.mora@omnilife.com>
2014-08-13 16:53:48 -0500

    Initial Commit

---
+++ guardar_archimg/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  guardar_archimg
+//
+//  Created by josue mora on 13/08/14.
+//  Copyright (c) 2014 josue mora. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ guardar_archimg/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  guardar_archimg
+//
+//  Created by josue mora on 13/08/14.
+//  Copyright (c) 2014 josue mora. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
